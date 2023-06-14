46447f40e5f2b3481eebded93c928a7550f15b7c
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2023-06-15 08:23:00 +1000

    Add a loop to generate DIVs

--- logic.js
+++ logic.js
@@ -2,10 +2,13 @@ function createGrid (){
 const container=document.querySelector("#container");
 const content=document.createElement('div');
 content.classList.add("content");
-content.textContent="Added a DIV, YAYYYY";
+content.textContent="1";
 container.appendChild(content);
 }
 
+for (let i=0; i<32; i++){
+    createGrid();
 
-createGrid();
+
+}
 
